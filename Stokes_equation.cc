6c0b254497500ef8bd85723d13fae3a8de6c18be
Ceteris90 <89290613+Ceteris90@users.noreply.github.com>
2021-08-27 13:24:23 +0200

    space changes

--- Stokes_equation.cc
+++ Stokes_equation.cc
@@ -18,37 +18,48 @@ namespace LA
 } // namespace LA
 #include <deal.II/lac/vector.h>
 #include <deal.II/lac/full_matrix.h>
+
 #include <deal.II/lac/solver_cg.h>
 #include <deal.II/lac/solver_gmres.h>
 #include <deal.II/lac/solver_minres.h>
+
 #include <deal.II/lac/affine_constraints.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
+
 #include <deal.II/lac/petsc_sparse_matrix.h>
 #include <deal.II/lac/petsc_vector.h>
 #include <deal.II/lac/petsc_solver.h>
 #include <deal.II/lac/petsc_precondition.h>
+
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/grid_tools.h>
+
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_renumbering.h>
 #include <deal.II/dofs/dof_tools.h>
+
 #include <deal.II/fe/fe_values.h>
 #include <deal.II/fe/fe_q.h>
 #include <deal.II/fe/fe_system.h>
+
 #include <deal.II/numerics/vector_tools.h>
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/error_estimator.h>
+
 #include <deal.II/base/utilities.h>
 #include <deal.II/base/conditional_ostream.h>
 #include <deal.II/base/index_set.h>
+
 #include <deal.II/lac/sparsity_tools.h>
 #include <deal.II/distributed/tria.h>
 #include <deal.II/distributed/grid_refinement.h>
 #include <deal.II/grid/grid_out.h>
+
 #include <cmath>
 #include <fstream>
 #include <iostream>
+
 namespace Step55
 {
   using namespace dealii;
@@ -118,6 +129,7 @@ namespace Step55
       preconditioner_S.vmult(dst.block(1), src.block(1));
     }
   } // namespace LinearSolvers
+	
   template <int dim>
   class RightHandSide : public Function<dim>
   {
